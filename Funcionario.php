6db42484ed7a1dc5e47639f0cd31dbc7d26956de
Kauan 3 <111802441+kauansilva001@users.noreply.github.com>
2023-05-02 17:04:58 -0300

    Add files via upload

---
+++ Funcionario.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Funcionario{
+        public $nome;
+        public $cpf;
+        public $endereco;
+        public $telefone;
+        public $salario;
+        public $funcao;
+        public $turnos;
+        public $email;
+
+        public function mostrarDados(){ 
+        }
+
+        public function calcularDesconto($descontodecimal){
+        }
+    }
+
+
+
+
+
+ ?>
